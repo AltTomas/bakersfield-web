8468dfafdf607366d360acd6a0b2e34f7670f2fb
alttomas <altruitomas@gmail.com>
2018-12-23 21:04:09 -0300

    changes

--- src/app/components/Navbar/components/NavbarButton/index.js
+++ src/app/components/Navbar/components/NavbarButton/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import { func, bool, string } from 'prop-types';
 
 import styles from './styles.module.scss';
 
@@ -8,11 +9,21 @@ class NavbarButton extends PureComponent {
     const { name, onPress, selected } = this.props;
 
     return (
-      <ButtonBase onClick={onPress} classes={{ root: `${styles.root} ${selected && styles.selected}` }}>
+      <ButtonBase
+        onClick={onPress}
+        disableRipple
+        classes={{ root: `${styles.root} ${selected && styles.selected}` }}
+      >
         {name}
       </ButtonBase>
     );
   }
 }
+
+NavbarButton.propTypes = {
+  name: string,
+  onPress: func,
+  selected: bool
+};
 
 export default NavbarButton;
